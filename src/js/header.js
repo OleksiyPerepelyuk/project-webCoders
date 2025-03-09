ad1f19d7e588355cd0dd5c9a79a6be39da8a15cd
Maryna <tromsa3122@mail.com>
2025-03-09 15:31:10 +0200

    add sectionHeader

--- src/js/header.js
+++ src/js/header.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const burgerBtn = document.querySelector(".burger-btn");
-  const navMenu = document.querySelector(".header-navigation");
-  const body = document.body;
 
-  burgerBtn.addEventListener("click", function () {
-    navMenu.classList.toggle("active");
-    body.classList.toggle("no-scroll");
-  });
-
-  // Закрытие меню при клике на ссылку
-  document.querySelectorAll(".header-menu-link").forEach(link => {
-    link.addEventListener("click", function () {
-      navMenu.classList.remove("active");
-      body.classList.remove("no-scroll");
-    });
-  });
-});
