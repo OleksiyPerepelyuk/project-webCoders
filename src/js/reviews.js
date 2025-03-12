dc2d7ffba7bfbfd57309b8e3dc2648b60105fd1e
AlexP <alexperepelyuk@gmail.com>
2025-03-12 19:02:22 +0100

    swiper modifide

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -33,7 +33,7 @@ function renderReviews(reviews) {
   const markup = reviews
     .map(
       review => `
-    <li class="swiper-slide" id="review">
+    <li class="swiper-slide reviews-swiper" id="review">
       <img class="reviewer-image" src="${review.avatar_url}" alt="user photo" width="48" height="48"/>
       <h3 class="reviewer-name">${review.author}</h3>
       <p class="review-text">${review.review}</p>
